54d7041dcc9c528c25ecc8ac1b36ac5f1ec2933f
megandreddy <87030448+megandreddy@users.noreply.github.com>
2021-07-25 21:54:56 -0400

    Get latest close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,6 +8,9 @@ import json
 # info inputs
 #
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price) #> $12,000.71
+
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 
 response = requests.get(request_url)
@@ -18,11 +21,11 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+latest_close = parsed_response["Time Series (Daily)"]["2021-07-23"]["4. close"]
 # breakpoint()
 #
 # info outputs
 #
-
 
 print("-------------------------")
 print("SELECTED SYMBOL: MSFT")
@@ -31,7 +34,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
